c3cbec4b9bc8cab96fe4cd89abcd4c8ab15bc174
gwl22 <giwook222@naver.com>
2016-12-12 17:03:24 +0900

    Enhanced Accuracy

--- project03/DTC_find_package.py
+++ project03/DTC_find_package.py
@@ -1,4 +1,4 @@
-# -*- coding: urf-8 -*-
+# -*- coding: utf-8 -*-
 
 import dask.dataframe as dd
 
@@ -7,18 +7,20 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 
+# random_number : num
+num = 0
+
 whole_train = dd.read_csv('dataset/train.csv',
                           parse_dates=['date_time', 'srch_ci', 'srch_co'])
 
-# random_number : num
-num = 0
 
 train_temp = whole_train.get_partition(num)
 pre_train = train_temp.head(len(train_temp))
 
 train = preprocess_dataset(pre_train).make_sample()
 
-x = train.drop(['orig_destination_distance',
+x = train.drop(['user_id',
+                'orig_destination_distance',
                 'user_location_city',
                 'user_location_region',
                 'posa_continent',
@@ -41,4 +43,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,
                                                     random_state=0)
 
 model = DecisionTreeClassifier(max_depth=100).fit(x_train, y_train)
-print accuracy_score(y_test, model.predict(x_test))
+print '='*10
+print 'Accuracy : {}'.format(accuracy_score(y_test, model.predict(x_test)))
+print '='*10
