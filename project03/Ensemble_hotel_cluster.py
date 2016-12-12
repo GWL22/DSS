ca10af72b205113a14a03447cc6f3f49a3e98c1a
gwl22 <giwook222@naver.com>
2016-12-12 17:04:21 +0900

    Adjust Nan Error

--- project03/Ensemble_hotel_cluster.py
+++ project03/Ensemble_hotel_cluster.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import dask.dataframe as dd
+import numpy as np
 
 from preprocessingdata import preprocess_dataset
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, VotingClassifier
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.cross_validation import train_test_split
 
-whole_train = dd.read_csv('dataset/train.csv',
-                          parse_dates=['date_time', 'srch_ci', 'srch_co'])
-
-# random_number : num
 num = 0
 
+whole_train = dd.read_csv('dataset/train.csv',
+                          parse_dates=['date_time',
+                                       'srch_ci',
+                                       'srch_co'])
 train_temp = whole_train.get_partition(num)
 pre_train = train_temp.head(len(train_temp))
+all_train = preprocess_dataset(pre_train).make_sample()
 
-train = preprocess_dataset(pre_train).make_sample()
+train = all_train.reset_index(np.random.permutation(all_train.index)) \
+                 .head(10000)
 
+train = train.set_index('index')
 
 x = train.drop('hotel_cluster', axis=1)
 y = train['hotel_cluster']
@@ -48,5 +52,5 @@ print 'model VC is ready'
 
 # print classification_report(y_test, model_predict4)
 print '='*10
-print 'Accuracy : {}'.format(accuracy_score(y_test, model5.predict(x_test)))
+print 'Accuracy : {}'.format(accuracy_score(y_test, model4.predict(x_test)))
 print '='*10
